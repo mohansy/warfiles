e309fc290c21071f4a7220cbe008cbecc70eedd6
mohan <test@Mohan.com>
2021-10-19 11:46:43 +0530

    files

---
+++ src/test/java/sbi/AppTest.java
@@ -0,0 +1,16 @@
+package sbi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Dummy Test
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
